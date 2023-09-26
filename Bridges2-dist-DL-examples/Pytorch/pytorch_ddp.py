2f3f91afacc2fbc549baa641fb670ee306799b69
meiyuw <meiyu.physics@gmail.com>
2023-09-25 20:05:46 -0400

    Update pytorch_ddp.py

--- Bridges2-dist-DL-examples/Pytorch/pytorch_ddp.py
+++ Bridges2-dist-DL-examples/Pytorch/pytorch_ddp.py
@@ -18,7 +18,7 @@ parser = argparse.ArgumentParser(description='Input values.')
 parser.add_argument('-nw', type=int, default=10, help='number of workers in dataloader')
 parser.add_argument('-bz', type=int, default=128, help='Batch size per replica')
 parser.add_argument('-image_size', type=int, default=128, help='Image size')
-parser.add_argument('-epoch_num', type=int, default=3, help='Number of training epochs')
+parser.add_argument('-epoch_num', type=int, default=5, help='Number of training epochs')
 parser.add_argument('-mp', action='store_true', help='Mixed Precision Training')
 parser.add_argument('-imagenet', action='store_true', help='Using Imagenet dataset')
 
