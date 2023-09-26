d4f41cd13e24228af7aa742e165683d72dfedadd
meiyuw <meiyu.physics@gmail.com>
2023-09-25 20:06:02 -0400

    Update tensorflow_dist.py

--- Bridges2-dist-DL-examples/Tensorflow/tensorflow_dist.py
+++ Bridges2-dist-DL-examples/Tensorflow/tensorflow_dist.py
@@ -6,7 +6,7 @@ import time
 import argparse
 
 parser = argparse.ArgumentParser(description='Input values.')
-parser.add_argument('-bz', type=int, default=64, help='Batch size per replica')
+parser.add_argument('-bz', type=int, default=128, help='Batch size per replica')
 parser.add_argument('-image_size', type=int, default=128, help='Image size')
 parser.add_argument('-epoch_num', type=int, default=5, help='Number of training epochs')
 parser.add_argument('-mp', action='store_true', help='Mixed Precision Training')
